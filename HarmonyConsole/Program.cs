3e367a640a266d446619272e51e41e766c09f78f
yinyongxian <yinyongxian@qq.com>
2025-12-28 16:28:49 +0800

    Harmony

--- HarmonyConsole/Program.cs
+++ HarmonyConsole/Program.cs
@@ -2,11 +2,19 @@
 using HarmonyData;
 using HarmonyLib;
 using HarmonyPatchLib;
+using System.Reflection;
 
 Console.WriteLine("Hello, World!");
 
 Harmony harmony = new("com.example.harmonypatch");
-harmony.PatchAll(typeof(HumanPatch).Assembly);
+//harmony.PatchAll(typeof(HumanPatch).Assembly);
+
+var original = typeof(Human).GetMethod("Greet");
+
+const BindingFlags BindingAttr = BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
+HarmonyMethod prefix = new(typeof(HumanPatch).GetMethod("Prefix", BindingAttr));
+HarmonyMethod postfix = new(typeof(HumanPatch).GetMethod("Postfix", BindingAttr));
+harmony.Patch(original, prefix, postfix, null, null);
 
 Human human = new()
 {
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # HarmonyDemo
+
+This is a simple demo project for the Harmony library. It showcases how to use Harmony to patch methods at runtime in a .NET application.  
+  
+[Harmony](https://github.com/pardeike/Harmony)
+  
+FastWin32
